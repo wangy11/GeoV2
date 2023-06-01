52a0ea12f6108703c60f1da2b4118883860c0e8f
wangyan <2679461129@qq.com>
2023-06-01 15:26:24 +0800

    fix: 取消代理，添加去除console

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,21 @@
 module.exports = {
-  publicPath: '/',
-  outputDir: 'dist',
-  productionSourceMap:false,
-//   devtool: false,
+  publicPath: "/",
+  outputDir: "dist",
+  productionSourceMap: false,
   devServer: {
-      port:8202,
-    //   https: true,
-      inline:false,
-      proxy: {
-          "/auth": {
-              target: "http://geohub.real4d.xyz",
-              secure: false,
-              changeOrigin: true,
-              pathRewrite: {
-                  "^/auth": ""
-              },
-            // headers : {'referer': 'https://geo.datav.aliyun.com/'}
-
-          }
-          
-      }
-
-  }, 
-
-}
+    host: "0.0.0.0",
+    port: 8202,
+    open: true,
+    inline: false,
+  },
+  //去除生产console.log
+  chainWebpack(config) {
+    config.optimization.minimizer("terser").tap((args) => {
+      args[0].parallel = 4;
+      args[0].terserOptions.compress.warnings = true;
+      args[0].terserOptions.compress.drop_debugger = true;
+      args[0].terserOptions.compress.drop_console = true;
+      return args;
+    });
+  },
+};
